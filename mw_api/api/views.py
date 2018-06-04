bf703c9fb0ac07876aa2ae2c3a0d9487adbc4047
Marlon Mendes <marlonciriatico@gmail.com>
2018-06-04 09:59:28 -0300

    Creating url, views for campi

--- mw_api/api/views.py
+++ mw_api/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import (Campus, )
+from .serializers import (CampusSerializer, )
+
+
+class CampiViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows campi to be viewed.
+    """
+    queryset = Campus.objects.all()
+    serializer_class = CampusSerializer
