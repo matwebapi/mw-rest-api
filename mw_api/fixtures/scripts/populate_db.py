28f1fc1fd4482cea27262aab00ad8d1258f32b83
Marlon Mendes <marlonciriatico@gmail.com>
2018-06-14 11:25:14 -0300

    Refactoring populate db script, Courses instances

--- mw_api/fixtures/scripts/populate_db.py
+++ mw_api/fixtures/scripts/populate_db.py
@@ -1,6 +1,16 @@
 from api.models import *
 
-gama = Campus.objects.get_or_create(name='Gama')
-darcy = Campus.objects.get_or_create(name='Darcy Ribeiro')
-planaltina = Campus.objects.get_or_create(name='Planaltina')
-ceilandia = Campus.objects.get_or_create(name='Ceilândia')
+print('Creating CAMPI\n\n')
+gama = Campus.objects.get_or_create(name='Gama')[0]
+darcy = Campus.objects.get_or_create(name='Darcy Ribeiro')[0]
+planaltina = Campus.objects.get_or_create(name='Planaltina')[0]
+ceilandia = Campus.objects.get_or_create(name='Ceilândia')[0]
+
+print('Creating Courses\n\n')
+PRESENCIAL = 'P'
+DIURNO = 'D'
+software = Course.objects.get_or_create(code=1635, name='ENGENHARIA DE SOFTWARE', genre=PRESENCIAL,
+                  shift=DIURNO, campus=gama)[0]
+
+eletronica = Course.objects.get_or_create(code=1601, name='ENGENHARIA ELETRÔNICA', genre=PRESENCIAL,
+                  shift=DIURNO, campus=gama)[0]
