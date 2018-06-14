1f4ac5996aa4d7a1ffbf1fa9757f48fa29a0c593
Marlon Mendes <marlonciriatico@gmail.com>
2018-06-14 11:24:44 -0300

    Courses model

--- mw_api/api/models.py
+++ mw_api/api/models.py
@@ -9,7 +9,25 @@ class Campus(models.Model):
         ('Ceilândia', 'Faculdade de Ceilândia'),
     )
 
-    name = models.CharField(max_length=50, choices = CAMPI)
+    name = models.CharField(max_length=50, choices=CAMPI)
 
     def __str__(self):
         return str(self.name)
+
+
+class Course(models.Model):
+    GENRE = (
+             ('D', 'Distância'),
+             ('P', 'Presencial'),
+            )
+
+    SHIFTS = (
+              ('D', 'Diurno'),
+              ('N', 'Noturno'),
+             )
+
+    code = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=256)
+    genre = models.CharField(max_length=50, choices=GENRE)
+    shift = models.CharField(max_length=50, choices=SHIFTS)
+    campus = models.ForeignKey(Campus, on_delete = models.CASCADE)
