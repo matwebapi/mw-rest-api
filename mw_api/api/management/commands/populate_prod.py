9b76c7de0d4bacfe2030d29aca948e9c9e8ef2c0
Marlon Mendes <marlonciriatico@gmail.com>
2018-06-15 00:04:28 -0300

    Creating departments from all json department files

--- mw_api/api/management/commands/populate_prod.py
+++ mw_api/api/management/commands/populate_prod.py
@@ -4,7 +4,7 @@ import os
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
-from api.models import (Department, )
+from api.models import (Campus, Department, )
 
 class Command(BaseCommand):
     help = 'Populate initial data for production API.'
@@ -70,6 +70,13 @@ class Json:
                                           extension=Json.EXTENSION)
         return jsons
 
+
+def debug(a, b):
+    print('\n' * 5)
+    print('DEBUG')
+    print('{} = {}'.format(a, b))
+    print('type({}) = {}'.format(a, type(b)))
+
 class DepartmentCreator:
     key_map = {
         'code': 'CODIGO',
@@ -78,22 +85,51 @@ class DepartmentCreator:
     }
 
     def __init__(self, json_data):
-        for value in DepartmentCreator.key_map.values():
-            assert(value in json_data.keys())
-        self.data = json_data
+        self.json_name = list(json_data.keys())[0]
+        self.data = json_data[self.json_name]
+        self.__set_campus_name()
 
-    def create_department(self):
+        for value in DepartmentCreator.key_map.values():
+            assert(value in self.data.keys())
+
+
+    def __set_campus_name(self):
+        PREFIX = 'DEPARTAMENTOS_'
+        assert(self.json_name.startswith(PREFIX))
+        self.campus_name = self.json_name[len(PREFIX):]
+
+    def __init_lists(self):
         tuples = []
+        lists = []
         for item in DepartmentCreator.key_map.items():
             key, key_json = item
             value = self.data[key_json]
+            if not isinstance(value, list):
+                value = [value]
+
             tuples.append((key, value))
-        kwargs = dict(tuples)
-        department, created = Department.objects.get_or_create(**kwargs)[0]
+            lists.append(value)
+
+        lists = list(zip(*lists))
+        self.lists = lists
+
+    def create_departments(self):
+        self.__init_lists()
+
+        for i in range(len(self.lists)):
+            self.__create_department_from_index(i)
+
+    def __create_department_from_index(self, i):
+        code, name, initials = self.lists[i]
+        campus = Campus.objects.get_or_create(name=self.campus_name)[0]
+
+        department, created = Department.objects.get_or_create(code=code, initials=initials,
+                                           name=name, campus=campus)
         if created:
-            print('Creating department: {}'.format(department.name))
+            print('Created DEPARTMENT [{}] successfully'.format(department.name))
         else:
-            print('Cant create department: {}'.format(department.name))
+            print('DEPARTMENT [{}] already existed.'.format(department.name))
+        return department
 
 class MiddleParser:
     DEPARTAMENTOS = 'DEPARTAMENTOS_'
@@ -105,14 +141,19 @@ class MiddleParser:
     def create_model_according_to_json_name(self, name):
         assert(name.endswith(Json.EXTENSION))
         data = Json.get_json_data(name)
-        root_name = list(data.keys())[0]
-        data = data[root_name]
+        root = list(data.keys())[0]
 
         if name.startswith(MiddleParser.DEPARTAMENTOS):
+            debug('data', data)
+            print('Creating DEPARTMENTS: {}'.format(root))
             dc = DepartmentCreator(data)
-            d = dc.create_department()
-            return
+            d = dc.create_departments()
+            if name != 'DEPARTAMENTOS_FGA.json':
+                return
+            else:
+                debug('FGA.data', data)
         elif name.startswith(MiddleParser.DISCIPLINAS):
-            print('Creating SUBJECT')
+            pass
+  #          print('Creating SUBJECT')
         else:
             raise Exception('File name not supported')
