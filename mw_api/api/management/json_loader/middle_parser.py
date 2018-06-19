e3719fd81c2476482815d61a9948c6fa6e39251f
Marlon Mendes <marlonciriatico@gmail.com>
2018-06-18 21:05:50 -0300

    Encapsulating lists creation inside MiddleParser class
    Signed-off-by: Marlon Mendes <marlonciriatico@gmail.com>
    Signed-off-by: Marlon Mendes <marlonciriatico@gmail.com>
    Co-authored-by: Rafaelbrg <rabragancaunb@gmail.com>
    Co-authored-by: FilipeKN4 <filipekn4@gmail.com>
    Co-authored-by: Nukdown <belchior.emanoel@gmail.com>

--- mw_api/api/management/json_loader/middle_parser.py
+++ mw_api/api/management/json_loader/middle_parser.py
@@ -1,35 +1,41 @@
 from api.management.json_loader.json_path import Json
 from api.management.json_loader .department_creator import DepartmentsCreator
 from api.management.json_loader .subject_creator import SubjectsCreator
-from api.management.json_loader import utils
+from api.management.json_loader.utils import debug
 
 
 class MiddleParser:
     DEPARTAMENTOS = 'DEPARTAMENTOS_'
     DISCIPLINAS = 'DISCIPLINAS_'
 
-    def __init__(self):
+    def __init__(self, json_names):
+        self.__init_models_lists(json_names)
         pass
 
-    def create_model_according_to_json_name(self, name):
-        assert(name.endswith(Json.EXTENSION))
+    def __init_models_lists(self, json_names):
+        self.department_names = []
+        self.subject_names = []
 
-        department_names = []
-        subject_names = []
+        for name in json_names:
+            assert(name.endswith(Json.EXTENSION))
+            if name.startswith(MiddleParser.DEPARTAMENTOS):
+                self.department_names.append(name)
+            elif name.startswith(MiddleParser.DISCIPLINAS):
+                self.subject_names.append(name)
+            else:
+                raise Exception('File name not supported')
 
-        if name.startswith(MiddleParser.DEPARTAMENTOS):
-            department_names.append(name)
-        elif name.startswith(MiddleParser.DISCIPLINAS):
-            subject_names.append(name)
-        else:
-            raise Exception('File name not supported')
-
-        for name in department_names:
+    def create_all_models(self):
+        for name in self.department_names:
             data = Json.get_json_data(name)
             dc = DepartmentsCreator(data)
             d = dc.create_departments()
 
-        for name in subject_names:
+            if name == 'DEPARTAMENTOS_FUP.json':
+                debug('name', name)
+                debug('data', data)
+
+        for name in self.subject_names:
             data = Json.get_json_data(name)
 
             sc = SubjectsCreator(data)
